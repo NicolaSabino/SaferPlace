22a7b113e3d2d3e745e16fe22669f6aaca20e12c
NicolaSabino <nicola.sabino94@gmail.com>
2016-05-15 16:46:04 +0200

    pagine statiche

---
+++ Demo_Statiche/js/index.js
@@ -0,0 +1,16 @@
+(function ($){
+  $(function(){
+
+    //abilito la sidenav collassabile
+    $('.button-collapse').sideNav();
+
+    //abilito l'effetto parallasse
+    $('.parallax').parallax();
+
+    //abilito le liste collasabili
+    $('.collapsible').collapsible({
+      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
