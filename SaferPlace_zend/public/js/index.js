f4cc4bd79081a616b9f5e3c8d216d92722dd257f
Peppep94 <gromani14@gmail.com>
2016-05-16 10:52:56 +0200

    Configurazione path zend

--- SaferPlace_zend/public/js/index.js
+++ SaferPlace_zend/public/js/index.js
@@ -3,6 +3,6 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-
+    $('.modal-trigger').leanModal();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
