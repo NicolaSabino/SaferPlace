66f890af148454c1aee7bdaf65902e2fed1b8077
NicolaSabino <nicola.sabino94@gmail.com>
2016-05-19 14:16:04 +0200

    view faq

---
+++ SaferPlace_zend/tests/application/controllers/FaqControllerControllerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+class FaqControllerControllerTest extends Zend_Test_PHPUnit_ControllerTestCase
+{
+
+    public function setUp()
+    {
+        $this->bootstrap = new Zend_Application(APPLICATION_ENV, APPLICATION_PATH . '/configs/application.ini');
+        parent::setUp();
+    }
+
+    public function testIndexAction()
+    {
+        $params = array('action' => 'index', 'controller' => 'FaqController', 'module' => 'default');
+        $urlParams = $this->urlizeOptions($params);
+        $url = $this->url($urlParams);
+        $this->dispatch($url);
+        
+        // assertions
+        $this->assertModule($urlParams['module']);
+        $this->assertController($urlParams['controller']);
+        $this->assertAction($urlParams['action']);
+        $this->assertQueryContentContains(
+            'div#view-content p',
+            'View script for controller <b>' . $params['controller'] . '</b> and script/action name <b>' . $params['action'] . '</b>'
+            );
+    }
+
+
+}
+
+
+
