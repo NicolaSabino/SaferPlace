8cde3a7628faaf8e55c5f12b42518bbe4c781bf4
NicolaSabino <nicola.sabino94@gmail.com>
2016-05-19 12:17:27 +0200

    model faq

---
+++ SaferPlace_zend/application/models/Faq.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ *  Mapper delle FAQ
+ */
+
+class Application_Model_Faq extends App_Model_Abstract
+{
+
+    /*
+     * Restituisce un oggetto rappresentante il set di faq presenti nel db
+     *
+     * @return Zend_Db_Table_Abstract
+     */
+    public function getFaqSet(){
+
+        return $this->getResource('Faq')->getAll();
+
+    }
+
+}
+
