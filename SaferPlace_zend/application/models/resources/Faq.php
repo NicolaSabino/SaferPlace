fbe7f55b8f41f23956cae7e9f198c2c9aa24bbd1
NicolaSabino <nicola.sabino94@gmail.com>
2016-05-19 12:17:27 +0200

    model faq

---
+++ SaferPlace_zend/application/models/resources/Faq.php
@@ -0,0 +1,21 @@
+<?php
+
+class Application_Model_DbTable_Faq extends Zend_Db_Table_Abstract
+{
+
+    /*
+     * Informazioni sulla tabella
+     */
+    protected $_name = 'faq';
+    protected $_primary ='id';
+    protected $_rowClass = 'Application_Resource_Faq_Item';
+
+    //seleziono tutte le tuple della tabella
+    public function getAll(){
+        $select = $this->select();
+        return $this->fetchAll($select);
+    }
+
+
+}
+
