41c82482286a4a06f11890d3ed1250ca4deb9454
NicolaSabino <nicola.sabino94@gmail.com>
2016-05-19 15:18:03 +0200

    modifiche su bootstrap.php

--- SaferPlace_zend/application/Bootstrap.php
+++ SaferPlace_zend/application/Bootstrap.php
@@ -10,29 +10,6 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
             $controller->setBaseUrl('/SaferPlace/SaferPlace_zend/public');
     }
 
-        //questo metodo serve a caricare le classi presenti dentro model
-    protected function _initAutoLoad(){
-        $modelLoader= new Zend_Application_Module_Autoloader(array(
-            'namespace' => '',
-            'basePath' => APPLICATION_PATH //È definito dentro public/index.php
-        ));
-
-        return $modelLoader;
-    }
-
-    protected function _initDbAdapter(){
-        $dbAdapter = Zend_Db::factory('PDO_mysql', array(
-            'host'     => 'localhost',
-            'username' => 'root',
-            'password' => '',
-            'dbname'   => 'sp_db'
-        ));
-        Zend_Db_Table::setDefaultAdapter($dbAdapter);
-
-    }
-
-
-/*
     protected function _initRequest()
         // Aggiunge un'istanza di Zend_Controller_Request_Http nel Front_Controller
         // che permette di utilizzare l'helper baseUrl() nel Bootstrap.php
@@ -44,10 +21,9 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $request = new Zend_Controller_Request_Http();
         $front->setRequest($request);
     }
+    
 
-
-*/
-  //loader di cucchia
+    //questo metodo serve a caricare le classi presenti dentro model
     protected function _initDefaultModuleAutoloader()
     {
         $loader = Zend_Loader_Autoloader::getInstance();
@@ -55,6 +31,8 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $this->getResourceLoader()
             ->addResourceType('modelResource','models/resources','Resource');
     }
+
+
 }
 
 
