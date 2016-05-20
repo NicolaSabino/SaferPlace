7a9c57d313cee3f33c193db41b364e05e3c51ecf
NicolaSabino <nicola.sabino94@gmail.com>
2016-05-20 15:12:39 +0200

    modifiche su dashboard

---
+++ SaferPlace_zend/application/models/Faq.php
@@ -0,0 +1,18 @@
+<?php
+
+class Application_Model_Faq extends App_Model_Abstract
+{
+
+    /*
+     * Restituisce un oggetto rappresentante il set di Faq presenti nel db
+     *
+     * @return Zend_Db_Table_Abstract
+     */
+    public function getFaqSet(){
+
+        return $this->getResource('Faq')->getAll();
+
+    }
+
+}
+
