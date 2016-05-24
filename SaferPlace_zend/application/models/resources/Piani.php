8c87f259abf9680b7480d62ed251c72c3acb051a
giulia0709 <giulia0709@hotmail.com>
2016-05-24 09:44:18 +0200

    sistemato bug dopo merge

--- SaferPlace_zend/application/models/resources/Piani.php
+++ SaferPlace_zend/application/models/resources/Piani.php
@@ -2,7 +2,6 @@
 class Application_Resource_Piani extends  Zend_Db_Table_Abstract
 {
     protected  $_name='piano';
-    protected $_rowClass='Application_Model_DbTable_Piani';
 
     public function getPianiByEdificio($edificio){
 
