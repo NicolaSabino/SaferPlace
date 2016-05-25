c20e1429a106ba6b7f8608ab98cb32ddbcaf0ab6
Peppep94 <gromani14@gmail.com>
2016-05-25 15:52:40 +0200

    sistemata segnalazione tranne insert nel db

--- SaferPlace_zend/application/models/Posizioni.php
+++ SaferPlace_zend/application/models/Posizioni.php
@@ -10,4 +10,8 @@ class Application_Model_Posizioni extends App_Model_Abstract
     public function getIdPosizioniByNumPianoStanzaSet($numPiano, $stanza){
         return $this->getResource('Posizioni')->getIdPosizioniBynumPianoStanza($numPiano, $stanza);
     }
+
+    public function getPosizioniByIdSet($id){
+        return $this->getResource('Posizioni')->getPosizioniById($id);
+    }
 }
