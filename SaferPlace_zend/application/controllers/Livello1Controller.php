f317b798517623e5e43c74920cdafb1c4e8b339a
giulia0709 <giulia0709@hotmail.com>
2016-06-08 09:57:40 +0200

    fix

--- SaferPlace_zend/application/controllers/Livello1Controller.php
+++ SaferPlace_zend/application/controllers/Livello1Controller.php
@@ -327,12 +327,6 @@ class Livello1Controller extends Zend_Controller_Action
         
     }
 
-
-  public function getModificaform()
-  {
-      return $this->getHelper('ModificaProfilo')->getForm($this->user, 1);
-  }
-
     public function verificamodificaAction()
         {
             $request = $this->getRequest();
