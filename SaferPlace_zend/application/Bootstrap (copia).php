8a2be264d46ebd6c42646d6b8e54a40c1ff8a992
AleZek <zechini.alessandro@gmail.com>
2016-06-09 16:12:53 +0200

    titolo bootstrap del server corretto

--- SaferPlace_zend/application/Bootstrap (copia).php
+++ SaferPlace_zend/application/Bootstrap (copia).php
@@ -31,7 +31,7 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $this->_view->headMeta()->setCharset('UTF-8');
         $this->_view->headMeta()->appendHttpEquiv('Content-Language', 'it-IT');
         $this->_view->headLink()->appendStylesheet($this->_view->baseUrl('css/materialize.css'));
-        $this->_view->headTitle('Corso di Tecnologie Web - Zend Project - Versione 6');
+        $this->_view->headTitle('SaferPlace');
     }
 
 
